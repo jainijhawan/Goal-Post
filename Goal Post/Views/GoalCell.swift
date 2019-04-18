0ca5fb6facef003fafea6e3a58d97c11302d430a
jainijhawan <jainijhawan@gmail.com>
2019-04-18 23:43:12 +0530

    Second Commit

---
+++ Goal Post/Views/GoalCell.swift
@@ -0,0 +1,24 @@
+//
+//  GoalCell.swift
+//  Goal Post
+//
+//  Created by Jai Nijhawan on 18/04/19.
+//  Copyright © 2019 Jai Nijhawan. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
