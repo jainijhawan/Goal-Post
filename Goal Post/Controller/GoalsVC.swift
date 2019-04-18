46bcd5862bae8f10e934f5e0ab2cbbac13e955f3
jainijhawan <jainijhawan@gmail.com>
2019-04-18 18:22:41 +0530

    First Commit

--- Goal Post/Controller/GoalsVC.swift
+++ Goal Post/Controller/GoalsVC.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GoalsVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func addGoalBtnWasPressed(_ sender: Any) {
+    }
+    
 }
 
