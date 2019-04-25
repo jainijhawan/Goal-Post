1e49b12827b760f5fa67406d031a77895cf2af3d
jainijhawan <jainijhawan@gmail.com>
2019-04-26 02:08:20 +0530

    Eleventh Commit

--- Goal Post/Controller/GoalsVC.swift
+++ Goal Post/Controller/GoalsVC.swift
@@ -77,8 +77,15 @@ extension GoalsVC : UITableViewDelegate, UITableViewDataSource{
             self.fetchCoreDataObjects()
             tableView.deleteRows(at: [indexPath], with: .automatic)
         }
+        
+        let addAction = UITableViewRowAction(style: .normal, title: "ADD 1") { (rowAction, indexPath) in
+            self.setProgress(atIndexPath: indexPath)
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+        
         deleteAction.backgroundColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
-        return [deleteAction]
+        addAction.backgroundColor = #colorLiteral(red: 0.9385011792, green: 0.7164435983, blue: 0.3331357837, alpha: 1)
+        return [deleteAction, addAction]
     }
     
 }
@@ -86,6 +93,23 @@ extension GoalsVC : UITableViewDelegate, UITableViewDataSource{
 
 
 extension GoalsVC{
+    
+    func setProgress(atIndexPath indexPath: IndexPath){
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else{return}
+        let chosenGoal = goals[indexPath.row]
+        
+        if chosenGoal.goalProgress < chosenGoal.goalCompletionValue{
+            chosenGoal.goalProgress = chosenGoal.goalProgress + 1
+        }else{
+            return
+        }
+        
+        do {
+            try managedContext.save()
+        } catch  {
+            debugPrint(error.localizedDescription)
+        }
+    }
     
     func removeGoal(atIndexPath indexPath: IndexPath){
         guard let managedContext = appDelegate?.persistentContainer.viewContext else{return}
--- Goal Post/Views/GoalCell.swift
+++ Goal Post/Views/GoalCell.swift
@@ -11,6 +11,7 @@ import UIKit
 class GoalCell: UITableViewCell {
 
     @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var completionView: UIView!
     
     @IBOutlet weak var goalTypeLbl: UILabel!
     @IBOutlet weak var goalProgressLbl: UILabel!
@@ -19,5 +20,12 @@ class GoalCell: UITableViewCell {
         self.goalDescriptionLbl.text = goal.goalDescription
         self.goalTypeLbl.text = goal.goalType
         self.goalProgressLbl.text = String(describing: goal.goalProgress)
+        
+        if goal.goalProgress == goal.goalCompletionValue
+        {
+            self.completionView.isHidden = false
+        }else{
+            self.completionView.isHidden = true
+        }
     }
 }
